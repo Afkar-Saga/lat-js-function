6e39b3be66ff520b616f1e9fa09b516604224e3b
Afkar-Saga <afkarsukmawanahmad@gmail.com>
2023-10-04 15:01:41 +0700

    init

---
+++ login.js
@@ -0,0 +1,22 @@
+// Regular Function
+function login() {
+    console.log("-=-=-=-=-=-=-=-=-=-")
+    console.log("       login       ")
+    console.log("-=-=-=-=-=-=-=-=-=-")
+    console.log("username: ")
+    console.log("pass: ")
+    console.log("-=-=-=-=-=-=-=-=-=-")
+}
+
+// Arrow Function
+const login2 = () => {
+    console.log("-=-=-=-=-=-=-=-=-=-")
+    console.log("       login       ")
+    console.log("-=-=-=-=-=-=-=-=-=-")
+    console.log("username: ")
+    console.log("pass: ")
+    console.log("-=-=-=-=-=-=-=-=-=-")
+}
+
+login()
+// login2()
